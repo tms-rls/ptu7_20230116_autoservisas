a863f3e3b2def045018470eb46ef6410b040187b
tms <tomas.rls@gmail.com>
2023-01-16 22:38:56 +0200

    koreguotas atvaizdavimas

--- autoservisas/autoservisas_uzd/models.py
+++ autoservisas/autoservisas_uzd/models.py
@@ -10,12 +10,24 @@ class AutomobilioModelis(models.Model):
     marke = models.CharField(verbose_name="Automobilio gamintojas", max_length=50, help_text="Įveskite automobilio gamintoją")
     modelis = models.CharField(verbose_name="Automobilio modelis", max_length=100, help_text="Įveskite automobilio modelį")
 
+    class Meta:
+        verbose_name_plural = "Automobilio modeliai"
+
+    def __str__(self):
+        return f"{self.marke} {self.modelis}"
+
 
 class Automobilis(models.Model):
     valstybinis_nr = models.CharField(verbose_name="Valstybinis numeris", max_length=10, help_text="Įveskite valstybinį numerį")
     automobilio_modelis = models.ForeignKey(to="AutomobilioModelis", on_delete=models.SET_NULL, null=True)
     vin_kodas = models.CharField(verbose_name="VIN kodas", max_length=50, help_text="Įveskite VIN kodą")
     klientas = models.CharField(verbose_name="Klientas", max_length=200, help_text="Įveskite klientą")
+
+    class Meta:
+        verbose_name_plural = "Automobiliai"
+
+    def __str__(self):
+        return f"{self.valstybinis_nr} {self.automobilio_modelis}"
 
 
 class Uzsakymas(models.Model):
@@ -24,10 +36,22 @@ class Uzsakymas(models.Model):
     automobilis = models.ForeignKey(to="Automobilis", on_delete=models.CASCADE)
     suma = models.CharField(verbose_name="Užsakymo suma", max_length=10)
 
+    class Meta:
+        verbose_name_plural = "Užsakymai"
+
+    def __str__(self):
+        return f"{self.automobilis} - {self.uuid}"
+
 
 class Paslauga(models.Model):
     pavadinimas = models.CharField(verbose_name="Paslaugos pavadinimas", max_length=200, help_text="Įveskite paslaugos pavadinimą")
     kaina = models.CharField(verbose_name="Paslaugos kaina", max_length=10, help_text="Įveskite paslaugos kainą")
+
+    class Meta:
+        verbose_name_plural = "Paslaugos"
+
+    def __str__(self):
+        return f"{self.pavadinimas}"
 
 
 class UzsakymoEilute(models.Model):
@@ -35,3 +59,6 @@ class UzsakymoEilute(models.Model):
     uzsakymas = models.ForeignKey(to="Uzsakymas", on_delete=models.CASCADE)
     kiekis = models.CharField(verbose_name="Kiekis", max_length=5, help_text="Įveskite kiekį")
     kaina = models.CharField(verbose_name="Kaina", max_length=10, help_text="Įveskite kainą")
+
+    class Meta:
+        verbose_name_plural = "Užsakymo eilutės"
