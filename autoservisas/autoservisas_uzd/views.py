685351c896ee434e4b480379018f48fd5cf09aa0
tms <tomas.rls@gmail.com>
2023-01-18 15:39:35 +0200

    ideta statistika

--- autoservisas/autoservisas_uzd/views.py
+++ autoservisas/autoservisas_uzd/views.py
@@ -2,8 +2,22 @@ from django.shortcuts import render
 
 # Create your views here.
 
+from django.shortcuts import render
 from django.http import HttpResponse
+from .models import (Automobilis,
+                     Uzsakymas,
+                     Paslauga)
 
 
 def index(request):
-    return HttpResponse("Labas, eikite į /admin/ puslapį")
+    paslaugu_kiekis = Paslauga.objects.all().count()
+    atliktu_uzsakymu_kiekis = Uzsakymas.objects.filter(statusas__exact=3).count()
+    automobiliu_kiekis = Automobilis.objects.all().count()
+
+    skaiciai = {
+        "paslaugu_kiekis": paslaugu_kiekis,
+        "atliktu_uzsakymu_kiekis": atliktu_uzsakymu_kiekis,
+        "automobiliu_kiekis": automobiliu_kiekis,
+    }
+
+    return render(request, 'statistika.html', context=skaiciai)
